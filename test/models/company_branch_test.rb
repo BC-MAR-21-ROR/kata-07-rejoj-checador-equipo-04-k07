37ac82b2de5f84c6a9a20c423133977967dc3911
Brayan DIaz C <brayandiazc@gmail.com>
2021-04-14 17:42:22 -0500

    feat(admin panel): activeadmin integrate to project Co-authored-by: Magic-Kowel <a20170094@utem.edu.mx> Co-authored-by: Carlos-Montiel <crmm17.cm@gmail.com>

--- test/models/company_branch_test.rb
+++ test/models/company_branch_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: company_branches
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class CompanyBranchTest < ActiveSupport::TestCase
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                :bigint           not null, primary key
+#  email             :string
+#  name              :string
+#  position          :string
+#  employee_number   :integer
+#  status            :boolean          default(TRUE)
+#  company_branch_id :bigint           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase

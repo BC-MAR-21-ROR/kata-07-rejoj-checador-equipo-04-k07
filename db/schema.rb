9a8efe307d449b954ebc811b118891f30acae39a
Brayan DIaz C <brayandiazc@gmail.com>
2021-04-14 16:48:34 -0500

    feat(companyBranches): created model companyBranch Co-authored-by: Magic-Kowel <a20170094@utem.edu.mx> Co-authored-by: Carlos-Montiel <crmm17.cm@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_222859) do
+ActiveRecord::Schema.define(version: 2021_04_14_214628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "company_branches", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"

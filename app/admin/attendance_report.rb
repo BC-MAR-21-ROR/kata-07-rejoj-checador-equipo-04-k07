84034f82231956066a2b652f7382b3dbeb52cc16
carlos <crmm17.cm@gmail.com>
2021-04-29 17:50:01 -0500

    export to csv

--- app/admin/attendance_report.rb
+++ app/admin/attendance_report.rb
@@ -26,8 +26,8 @@ ActiveAdmin.register_page 'Attendance Report' do
       paginated_collection(collection, download_links: false) do
         table_for(collection) do
           column :day
-          column(:check_type) { |type| type.check_type.zero? ? 'in' : 'out' }
-          column :time
+          column(:check_type) { |employee| employee.check_type.zero? ? 'in' : 'out' }
+          column(:time) { |employee| employee.time.to_datetime.in_time_zone }
           column :employee_number
           column :name
           column :private_number
@@ -35,5 +35,24 @@ ActiveAdmin.register_page 'Attendance Report' do
         end
       end
     end
+  end
+  page_action :import_csv, method: :get do
+    day = (params[:day] || Date.today.strftime('%Y-%m-%d'))
+    employees = Employee.attendances(day)
+    csv = CSV.generate(encoding: 'UTF-8') do |csv_file|
+      csv_file << ['Day', 'Check type', 'Time', 'Employee Number', 'Name', 'Private Number', 'Email']
+      employees.each do |u|
+        check_type = u.check_type.zero? ? 'in' : 'out'
+        time = u.time.to_datetime.in_time_zone
+        csv_file << [u.day, check_type, time, u.employee_number, u.name, u.private_number, u.email]
+      end
+    end
+    send_data csv.encode('UTF-8'), type: 'text/csv; charset=UTF-8; header=present',
+                                   disposition: 'attachment; filename=attendance_report.csv'
+  end
+
+  action_item :add do
+    link_to 'Export to CSV', admin_attendance_report_import_csv_path(day: params[:day]),
+            method: :get
   end
 end
